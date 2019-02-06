07bd313d2691f3586059a3239f5e6ad6b2f4c663
RD77 <dominguez7777@gmail.com>
2019-02-06 10:50:09 -0800

    Travis Assignment

---
+++ src/Multiply.java
@@ -0,0 +1,12 @@
+
+public class Multiply {
+		double lhs;
+		double rhs;
+	    public Multiply(double lhs, double rhs) {
+	        this.lhs = lhs;
+	        this.rhs = rhs;
+	    }
+	    public double mult() {
+	    	return lhs*rhs;
+	    }
+}
